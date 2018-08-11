1549965d11a51ff52929e61d97f5761d3b9e6b25
Asish Shaji Thomas <mrkai1253@gmail.com>
2018-08-11 11:25:03 +0530

    1

--- blockchain.py
+++ blockchain.py
@@ -16,6 +16,47 @@ class BlockChain:
         block = {
             'index' : len(self.chain) + 1,
             'timestamp' : datetime.datetime.now(),
-            
-        }
+            'proof' : proof,
+            'prev_hash' : prev_hash,
+            }
+        self.chain.append(block)
+
+        return block
+
+    def get_prev_block(self):
+        return self.chain[-1]
+
+#The proof of work miners have to solve
+    def pow(self, prev_proof):
+        #checking if the new proof is below certain value
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - prev_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == "0000":
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+    
+    def hash(self, block):
+        #converting dictionary to block 
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
+
+    def is_chain_valid(self, chain):
+        prev_block = chain[0]
+        block_i = 1 #block index
+        while block_i < len(chain):
+            block = chain[block_i]
+            if block['prev_hash'] != self.hash(prev_block):
+                return False
+            prev_proof = prev_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - prev_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+
         
+
